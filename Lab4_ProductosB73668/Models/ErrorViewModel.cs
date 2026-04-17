ad0a47b1999aba220aa3495686b182f5dc8c2f6e
Isaura Hernández González <deilin99hg@gmail.com>
2026-04-17 17:22:20 -0600

    Agregar archivos de proyecto.

---
+++ Lab4_ProductosB73668/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab4_ProductosB73668.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
