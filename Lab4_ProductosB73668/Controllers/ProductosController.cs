927aeacb98030cecd3eff44ec3892e3725ac0467
isaura99 <deilin99hg@gmail.com>
2026-04-17 18:21:41 -0600

    Agrega ProductosController con acciones CRUD y sus vistas

---
+++ Lab4_ProductosB73668/Controllers/ProductosController.cs
@@ -0,0 +1,74 @@
+﻿using Lab4_ProductosB73668.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4_ProductosB73668.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Producto> productos = ProductoRepositorio.ObtenerTodos();
+            return View(productos);
+        }
+
+        public IActionResult Detalles(int id)
+        {
+            Producto? producto = ProductoRepositorio.ObtenerPorId(id);
+            if (producto == null) return RedirectToAction("Index");
+
+            return View(producto);
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Producto producto)
+        {
+            if (!ModelState.IsValid) return View(producto);
+
+            ProductoRepositorio.Agregar(producto);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            Producto? producto = ProductoRepositorio.ObtenerPorId(id);
+            if (producto == null) return RedirectToAction("Index");
+
+            return View(producto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Producto producto)
+        {
+            if (!ModelState.IsValid) return View(producto);
+
+            ProductoRepositorio.Actualizar(producto);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            Producto? producto = ProductoRepositorio.ObtenerPorId(id);
+            if (producto == null) return RedirectToAction("Index");
+
+            return View(producto);
+        }
+
+        [HttpPost, ActionName("Eliminar")]
+        [ValidateAntiForgeryToken]
+        public IActionResult EliminarConfirmado(int id)
+        {
+            ProductoRepositorio.Eliminar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
